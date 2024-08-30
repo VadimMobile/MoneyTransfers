023831b6c3d24a3fed4831ed6bf8a0e62826cc65
Vadim Mobile <qwerty901cx@gmail.com>
2024-08-30 22:02:15 +0300

    formatted the code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,12 @@
 fun main() {
     val commission = 0.75
     val minCommission = 35
-   val amount = 10000
-   val amount2 = 1000
-   val sumCommission = amount / 100 * commission
+    val amount = 10000
+    val amount2 = 1000
+    val sumCommission = amount / 100 * commission
     val sumCommission2 = amount2 / 100 * commission
     val sizeCommission = if (sumCommission > minCommission) sumCommission else minCommission
-  val sizeCommission2 = if (sumCommission2 > minCommission) sumCommission2 else minCommission
+    val sizeCommission2 = if (sumCommission2 > minCommission) sumCommission2 else minCommission
 
     println(sizeCommission)
     println(sizeCommission2)
